7b05c031fdd5e1496fd0c2a0bdcdd8f7522e08d4
Dmytro Svystun <dmytro.svystun@storable.com>
2023-07-29 00:37:43 +0300

    throw an error when the response is not parsed moved getcontent into helper

--- Mailjet.Client/IMailjetClient.cs
+++ Mailjet.Client/IMailjetClient.cs
@@ -15,11 +15,15 @@ namespace Mailjet.Client
         /// <summary>
         /// Sends a single transactional email using send API v3.1
         /// </summary>
+        /// <exception cref="MailjetClientConfigurationException">Thrown when email count exceeds the max allowed number</exception>
+        /// <exception cref="MailjetServerException">Thrown when generic error returned from the server</exception>
         Task<TransactionalEmailResponse> SendTransactionalEmailAsync(TransactionalEmail transactionalEmail, bool isSandboxMode = false, bool advanceErrorHandling = true);
 
         /// <summary>
         /// Sends transactional emails using send API v3.1
         /// </summary>
+        /// <exception cref="MailjetClientConfigurationException">Thrown when email count exceeds the max allowed number</exception>
+        /// <exception cref="MailjetServerException">Thrown when generic error returned from the server</exception>
         Task<TransactionalEmailResponse> SendTransactionalEmailsAsync(IEnumerable<TransactionalEmail> transactionalEmails, bool isSandboxMode = false, bool advanceErrorHandling = true);
     }
 }
---
+++ Mailjet.Tests/HttpContentHelperTests.cs
@@ -0,0 +1,101 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Helpers;
+using Mailjet.Client.TransactionalEmails.Response;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Mailjet.Tests
+{
+    [TestClass]
+    public class HttpContentHelperTests
+    {
+        [TestMethod]
+        public async Task GetContentAsync_WhenContentIsNull_ReturnsStatusCode()
+        {
+            // arrange
+            HttpStatusCode expectedStatusCode = HttpStatusCode.OK;
+
+            // act
+            var result = await HttpContentHelper.GetContentAsync(new HttpResponseMessage { StatusCode = expectedStatusCode });
+            HttpStatusCode statusCode = (HttpStatusCode) Enum.Parse(typeof(HttpStatusCode), result.Value<string>("StatusCode"));
+
+            // assert
+            Assert.AreEqual(expectedStatusCode, statusCode);
+        }
+
+        [TestMethod]
+        public async Task GetContentAsync_WhenContentNotNull_ParsesMessagesCorrectly()
+        {
+            // arrange
+            HttpResponseMessage response = 
+                GetHttpResponse(HttpStatusCode.BadRequest, "{\"Messages\":[{\"Status\":\"error\",\"Errors\":[{\"ErrorIdentifier\":\"6c1d35cb-3de8-495c-9d0e-0d563d2671da\",\"ErrorCode\":\"mj-0007\",\"StatusCode\":400,\"ErrorMessage\":\"You must provide at least one item in the collection.\",\"ErrorRelatedTo\":[\"To\"]}]}]}");
+
+            // act
+            var result = await HttpContentHelper.GetContentAsync(response);
+
+            // assert
+            Assert.IsTrue(result.Value<JArray>(nameof(TransactionalEmailResponse.Messages)).Any());
+        }
+
+        [TestMethod]
+        public async Task GetContentAsync_WhenContentIsGenericError_ReturnsErrorInfo()
+        {
+            // arrange
+            HttpResponseMessage response = 
+                GetHttpResponse(HttpStatusCode.Unauthorized, "{\"ErrorIdentifier\":\"e3f64f47-c99a-4a5a-b054-3ce1c5c7e4ce\",\"StatusCode\":401,\"ErrorMessage\":\"API key authentication/authorization failure. You may be unauthorized to access the API or your API key may be expired. Visit API keys management section to check your keys.\"}");
+
+            // act
+            var result = await HttpContentHelper.GetContentAsync(response);
+            string erroInfo = result.Value<string>(MailjetConstants.ErrorInfo);
+
+            // assert
+            Assert.IsNotNull(erroInfo);
+        }
+
+        [TestMethod]
+        public async Task GetContentAsync_WhenContentIsTooManyRequests_ReturnsCorrectErrorInfo()
+        {
+            // arrange
+            HttpResponseMessage response = GetHttpResponse((HttpStatusCode)429, "{\"ErrorIdentifier\":\"e3f64f47-c99a-4a5a-b054-3ce1c5c7e4ce\",\"StatusCode\":429,\"ErrorMessage\":\"Error!\"}");
+
+            // act
+            var result = await HttpContentHelper.GetContentAsync(response);
+            string erroInfo = result.Value<string>(MailjetConstants.ErrorInfo);
+
+            // assert
+            Assert.AreEqual(MailjetConstants.TooManyRequestsMessage, erroInfo);
+        }
+
+        [TestMethod]
+        public async Task GetContentAsync_WhenContentIsInternalServerError_ReturnsCorrectErrorInfo()
+        {
+            // arrange
+            HttpResponseMessage response = GetHttpResponse(HttpStatusCode.InternalServerError, "{\"ErrorIdentifier\":\"e3f64f47-c99a-4a5a-b054-3ce1c5c7e4ce\",\"StatusCode\":500,\"ErrorMessage\":\"Error!\"}");
+
+            // act
+            var result = await HttpContentHelper.GetContentAsync(response);
+            string erroInfo = result.Value<string>(MailjetConstants.ErrorInfo);
+
+            // assert
+            Assert.AreEqual(MailjetConstants.InternalServerErrorGeneralMessage, erroInfo);
+        }
+
+        private static HttpResponseMessage GetHttpResponse(HttpStatusCode expectedStatusCode, string contentString)
+        {
+            HttpResponseMessage response = new HttpResponseMessage
+            {
+                StatusCode = expectedStatusCode,
+                Content = new StringContent(contentString)
+            };
+
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            return response;
+        }
+    }
+}
